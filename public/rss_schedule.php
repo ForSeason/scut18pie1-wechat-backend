90c222ba0683562a04eeb4d0553a5aa41798d747
forseason <ysy604822@live.com>
2019-08-24 01:06:09 +0800

    尝试修正rss源和公众号课表双标问题2

--- public/rss_schedule.php
+++ public/rss_schedule.php
@@ -12,9 +12,8 @@ $jw = new JW($weixinID, 23333);
 $thisweek = date("W", time()) - WEEK_START;
 $rss = new Rss();
 
-$model = new Models\Model();
 $sql = "SELECT day,name,room,period,teacher,region FROM schedule WHERE weixinID=? AND week=?";
-$stmt = $model->link->prepare($sql);
+$stmt = $jw->link->prepare($sql);
 $stmt->execute([$weixinID, $thisweek]);
 $arr = $stmt->fetchAll(\PDO::FETCH_ASSOC | \PDO::FETCH_GROUP);
 
@@ -42,7 +41,7 @@ $item = [
 ];
 $rss->item($item);
 
-$next_class = JW::next_class($arr);
+$next_class = next_class($arr);
 if ($next_class[0]) {
     $item = [
         'title' => '下一节课',
@@ -104,3 +103,36 @@ $item = [
 ];
 if (!$bo) $rss->item($item);
 echo $rss;
+
+
+
+
+public function next_class($schedule) {
+    $week_days = array(
+        '星期零',
+        '星期一', 
+        '星期二', 
+        '星期三', 
+        '星期四', 
+        '星期五', 
+        '星期六', 
+        '星期日' 
+    );
+    $now_time = (int)date("Hi",time());
+    $now_day  = (int)date("N",time());
+    if (!array_key_exists($week_days[$now_day], $schedule)) return [false, '今天没有课哦～'];
+    foreach ($schedule[$week_days[$now_day]] as $lesson) {
+        $start_period_str = $lesson['period'];
+        $pattern = '/.*?(\d).*/';
+        $start_period = (int)preg_replace($pattern, '$1', $start_period_str);
+        $schedule_time = json_decode(SCHEDULE_TIME, true);
+        if ($schedule_time[$lesson['region']][$start_period] > $now_time) {
+            $res = $lesson['name'].' '.
+                $lesson['room'].' '.
+                $lesson['period'].' '.
+                $lesson['teacher'];
+            return [true, $res];
+        }
+    }
+    return [false, '今天已经没课啦！'];
+}
